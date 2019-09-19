e5c430b703d19c47e0e9b1cdb31998d8a0a94164
HK Yoon <hkyoon85@gmail.com>
2019-09-19 11:10:28 +0900

    Add example component with hook

---
+++ src/components/Counter/index.jsx
@@ -0,0 +1,24 @@
+// React Hooks Example
+import React, { useState } from "react";
+
+export default function Counter() {
+  const [count, setCount] = useState(0);
+
+  function handleAlertClick() {
+    setTimeout(() => {
+      alert('You clicked on: ' + count);
+    }, 3000);
+  }
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
+      <button onClick={handleAlertClick}>
+        Show alert
+      </button>
+    </div>
+  );
+}
--- stories/index.tsx
+++ stories/index.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import Button from "../src/components/Button";
+import Counter from "../src/components/Counter";
 
 storiesOf("Button", module)
   .add("a button", () => <Button />)
   .add("w/ a label", () => <Button label="Label" />);
+
+storiesOf("Counter", module)
+  .add("Counter", () => <Counter />);
