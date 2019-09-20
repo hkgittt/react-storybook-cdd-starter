8d612f88b0ec29eb2f78955d2d39e398e120b237
HK Yoon <hkyoon85@gmail.com>
2019-09-20 16:39:24 +0900

    Enable snapshot testing

--- src/components/Button/button.test.js
+++ src/components/Button/button.test.js
@@ -2,9 +2,22 @@ import React from 'react';
 import Button from './';
 import renderer from 'react-test-renderer';
 
-it('renders correctly', () => {
-  const tree = renderer
-    .create(<Button label="Facebook" />)
-    .toJSON();
-  expect(tree).toMatchSnapshot();
+describe('Sample test', () => {
+  it('confirms the obvious', () => {
+    expect(true).toEqual(true);
+  });
+
+  it('renders google button correctly', () => {
+    const tree = renderer
+      .create(<Button label="Google" />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders facebook button correctly', () => {
+    const tree = renderer
+      .create(<Button label="Facebook" />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
